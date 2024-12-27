5bb27a6d8d12dda7f90e8394bb8c71d16b17cc9a
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-27 10:04:23 +0530

    data try to be add

---
+++ data.js
@@ -0,0 +1,39 @@
+[
+    {
+      "Sr_NO": 1,
+      "image": "Agate Ornament",
+      "Productname": "RD397S",
+      "Qty": 1,
+      "Size": {
+        "length": 120,
+        "width": 70,
+        "height": 190
+      },
+      "Price": 89999
+    },
+    {
+      "Sr_NO": 1,
+      "image": "Agate Ornament",
+      "Productname": "RD397L",
+      "Qty": 1,
+      "Size": {
+        "length": 120,
+        "width": 70,
+        "height": 190
+      },
+      "Price": 89999
+    },
+    {
+      "Sr_NO": 2,
+      "image": "RESIN SCULPTURE",
+      "Productname": "SM-BJ721",
+      "Qty": 1,
+      "Size": {
+        "length": 130,
+        "width": 70,
+        "height": 260
+      },
+      "Price": 15999
+    }
+  ]
+  
