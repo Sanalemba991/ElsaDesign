d1d879150e54252027998f8a059cb19f1a697f3b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-27 15:15:07 +0530

    add data

--- data.js
+++ data.js
@@ -1,39 +1,447 @@
-[
-    {
-      "Sr_NO": 1,
-      "image": "Agate Ornament",
-      "Productname": "RD397S",
-      "Qty": 1,
-      "Size": {
-        "length": 120,
-        "width": 70,
-        "height": 190
+const data = {
+    products: [
+      {
+        id: 1,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "AGATE ORNAMENT",
+            "ProductName": "RD3927S",
+            "Quantity": 1,
+            "Size": "120X70X190",
+            "OnlinePrice": null,
+            "Price": 8999
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "AGATE ORNAMENT",
+            "ProductName": "RD3927L",
+            "Quantity": 1,
+            "Size": "120X70X260",
+            "OnlinePrice": null,
+            "Price": 8999
+          }
+        ]
       },
-      "Price": 89999
-    },
-    {
-      "Sr_NO": 1,
-      "image": "Agate Ornament",
-      "Productname": "RD397L",
-      "Qty": 1,
-      "Size": {
-        "length": 120,
-        "width": 70,
-        "height": 190
+      {
+        id: 2,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "RESIN SCULPTURE",
+            "ProductName": "SM-BJ721",
+            "Quantity": 1,
+            "Size": "450X200X1007",
+            "OnlinePrice": null,
+            "Price": 15999
+          }
+        ]
       },
-      "Price": 89999
-    },
-    {
-      "Sr_NO": 2,
-      "image": "RESIN SCULPTURE",
-      "Productname": "SM-BJ721",
-      "Qty": 1,
-      "Size": {
-        "length": 130,
-        "width": 70,
-        "height": 260
+      {
+        id: 3,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL FRUIT DECOR",
+            "ProductName": "RD5713",
+            "Quantity": 1,
+            "Size": "80X110X280",
+            "OnlinePrice": null,
+            "Price": 18999
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL FRUIT DECOR",
+            "ProductName": "RD5714",
+            "Quantity": 1,
+            "Size": "80X130X250",
+            "OnlinePrice": null,
+            "Price": 18999
+          }
+        ]
       },
-      "Price": 15999
-    }
-  ]
+      {
+        id: 4,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "GLOBAL TABLE DECOR",
+            "ProductName": "RD3964L",
+            "Quantity": 1,
+            "Size": "240X510",
+            "OnlinePrice": null,
+            "Price": 12999
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "GLOBAL TABLE DECOR",
+            "ProductName": "RD3964S",
+            "Quantity": 1,
+            "Size": "240X450",
+            "OnlinePrice": null,
+            "Price": 12999
+          }
+        ]
+      },
+      {
+        id: 5,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "ARTISTIC SWAN",
+            "ProductName": "DECOR",
+            "Quantity": 1,
+            "Size": null,
+            "OnlinePrice": null,
+            "Price": 10500
+          }
+        ]
+      },
+      {
+        id: 6,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "METAL MOUNTAIN",
+            "ProductName": "SM-Z s278",
+            "Quantity": 1,
+            "Size": "280X80X320",
+            "OnlinePrice": null,
+            "Price": 14999
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "METAL MOUNTAIN",
+            "ProductName": "SM-Z s279",
+            "Quantity": 1,
+            "Size": "280X70X270",
+            "OnlinePrice": null,
+            "Price": 14999
+          }
+        ]
+      },
+      {
+        id: 7,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL FLOWER",
+            "ProductName": "RD5712L",
+            "Quantity": 1,
+            "Size": "230X230X480",
+            "OnlinePrice": 6750,
+            "Price": 20999
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL FLOWER",
+            "ProductName": "RD5712S",
+            "Quantity": 1,
+            "Size": "230X230X410",
+            "OnlinePrice": 6750,
+            "Price": 20999
+          }
+        ]
+      },
+      {
+        id: 8,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "RESIN ORNAMENT",
+            "ProductName": "SM-Z s311",
+            "Quantity": 1,
+            "Size": "140X120X320",
+            "OnlinePrice": 16649,
+            "Price": 18499
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "RESIN ORNAMENT",
+            "ProductName": "SM-Z s312",
+            "Quantity": 1,
+            "Size": "200X100X180",
+            "OnlinePrice": 16649,
+            "Price": 18499
+          }
+        ]
+      },
+      {
+        id: 9,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CAKE STAND SNACKS PLATTER",
+            "ProductName": "DECOR",
+            "Quantity": 1,
+            "Size": "200X100X180",
+            "OnlinePrice": null,
+            "Price": 11999
+          }
+        ]
+      },
+      {
+        id: 10,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "DEER STATUE",
+            "ProductName": "RD5799L",
+            "Quantity": 1,
+            "Size": "140X210X370",
+            "OnlinePrice": null,
+            "Price": 26999
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "DEER STATUE",
+            "ProductName": "RD5799S",
+            "Quantity": 1,
+            "Size": "160X70X280",
+            "OnlinePrice": null,
+            "Price": 26999
+          }
+        ]
+      },
+      {
+        id: 11,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "DEER STATUE 2",
+            "ProductName": "RD5543L",
+            "Quantity": 1,
+            "Size": "220X190X535",
+            "OnlinePrice": "August Crystal Brass Deer Centrepiece Rs 28500 Glinded Lavender Serenity Ornament:Rs 17329 Set of 2:Rs 17329 Large:Rs 9874 Small:Rs 7456",
+            "Price": 20499
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "DEER STATUE 2",
+            "ProductName": "RD5543S",
+            "Quantity": 1,
+            "Size": "290X180X380",
+            "OnlinePrice": "August Crystal Brass Deer Centrepiece Rs 28500 Glinded Lavender Serenity Ornament:Rs 17329 Set of 2:Rs 17329 Large:Rs 9874 Small:Rs 7456",
+            "Price": 20499
+          }
+        ]
+      },
+      {
+        id: 12,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL CRANE BIRD",
+            "ProductName": "RD6151",
+            "Quantity": 1,
+            "Size": "130X85X360",
+            "OnlinePrice": "BUY ATORES BRASS CRAN STATUE MODERN FIGURINE SCULPTURE FOR HOME Rs:9721",
+            "Price": 19599
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL CRANE BIRD",
+            "ProductName": "RD6152",
+            "Quantity": 1,
+            "Size": "135X80X10",
+            "OnlinePrice": "BUY ATORES BRASS CRAN STATUE MODERN FIGURINE SCULPTURE FOR HOME Rs:9721",
+            "Price": 19599
+          }
+        ]
+      },
+      {
+        id: 13,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "LEAF ORNAMENT DECOR",
+            "ProductName": "RD5894L",
+            "Quantity": 1,
+            "Size": "210X120X470",
+            "OnlinePrice": null,
+            "Price": 18499
+          },
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "LEAF ORNAMENT DECOR",
+            "ProductName": "RD5894S",
+            "Quantity": 1,
+            "Size": "220X160X390",
+            "OnlinePrice": null,
+            "Price": 18499
+          }
+        ]
+      },
+      {
+        id: 14,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "GLASS FISH VASE",
+            "ProductName": "DECOR",
+            "Quantity": 1,
+            "Size": null,
+            "OnlinePrice": null,
+            "Price": 13599
+          }
+        ]
+      },
+      {
+        id: 15,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL WINE HOLDER",
+            "ProductName": "RD6149",
+            "Quantity": 1,
+            "Size": "240X90X300",
+            "OnlinePrice": null,
+            "Price": 8999
+          }
+        ]
+      },
+      {
+        id: 16,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "GOLDEN HORSE",
+            "ProductName": "RD6789",
+            "Quantity": 1,
+            "Size": "180X100X350",
+            "OnlinePrice": null,
+            "Price": 22999
+          }
+        ]
+      },
+      {
+        IDBCursor: 17,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "SILVER ELEPHANT",
+            "ProductName": "RD6790",
+            "Quantity": 1,
+            "Size": "150X90X300",
+            "OnlinePrice": null,
+            "Price": 21999
+          }
+        ]
+      },
+      {
+        id: 18,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "BRONZE LION",
+            "ProductName": "RD6791",
+            "Quantity": 1,
+            "Size": "200X120X400",
+            "OnlinePrice": null,
+            "Price": 23999
+          }
+        ]
+      },
+      {
+        id: 19,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "MARBLE DRAGON",
+            "ProductName": "RD6792",
+            "Quantity": 1,
+            "Size": "220X140X380",
+            "OnlinePrice": null,
+            "Price": 24999
+          }
+        ]
+      },
+      {
+        id: 20,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "COPPER PANTHER",
+            "ProductName": "RD6793",
+            "Quantity": 1,
+            "Size": "240X150X350",
+            "OnlinePrice": null,
+            "Price": 25999
+          }
+        ]
+      },
+      {
+        id: 21,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "WOODEN TIGER",
+            "ProductName": "RD6794",
+            "Quantity": 1,
+            "Size": "250X160X360",
+            "OnlinePrice": null,
+            "Price": 26999
+          }
+        ]
+      },
+      {
+        id: 22,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "IVORY GIRAFFE",
+            "ProductName": "RD6795",
+            "Quantity": 1,
+            "Size": "230X140X370",
+            "OnlinePrice": null,
+            "Price": 27999
+          }
+        ]
+      },
+      {
+        id: 23,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "STEEL WOLF",
+            "ProductName": "RD6796",
+            "Quantity": 1,
+            "Size": "240X150X380",
+            "OnlinePrice": null,
+            "Price": 28999
+          }
+        ]
+      },
+      {
+        id: 24,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CRYSTAL BEAR",
+            "ProductName": "RD6797",
+            "Quantity": 1,
+            "Size": "260X160X390",
+            "OnlinePrice": null,
+            "Price": 29999
+          }
+        ]
+      },
+      {
+        id: 25,
+        items: [
+          {
+            "ProductPicture": "PICTURE",
+            "Name": "CERAMIC VASES",
+            "ProductName": "RD3875S",
+            "Quantity": 1,
+            "Size": "200X100X350",
+            "OnlinePrice": null,
+            "Price": 18299
+          }
+        ]
+      }
+    ]
+  };
+  
+  export default data;
   
