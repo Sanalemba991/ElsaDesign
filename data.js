84fa706f263e6cac019a293fe26dfcd184465104
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-30 17:46:51 +0530

    worked it finally

--- data.js
+++ data.js
@@ -2,333 +2,276 @@ const data = {
   products: [
     {
       id: 1,
-      items: {
-        ProductPicture: "/images/1.png",
-        ProductName: "AGATE ORNAMENT",
-        ModelNumber: "RD3927S and RD3927L ",
-        Quantity: "1 set of 2",
-        Size: "120X70X190 and 120X70X260 ",
-        Price: 8999,
-      },
+
+      ProductPicture: "/images/1.jpeg",
+      ProductName: "AGATE ORNAMENT",
+      ModelNumber: "RD3927S and RD3927L ",
+      Quantity: "1 set of 2",
+      Size: "120X70X190 and 120X70X260 ",
+      Price: 8999,
     },
     {
       id: 2,
-      items: [
-        {
-          ProductPicture: "/images/2.png",
-          ProductName: "RESIN SCULPTURE",
-          ModelNumber: "SM-BJ721",
-          Quantity: "1",
-          Size: "450X200X1007",
 
-          Price: 15999,
-        },
-      ],
+      ProductPicture: "/images/2.png",
+      ProductName: "RESIN SCULPTURE",
+      ModelNumber: "SM-BJ721",
+      Quantity: "1",
+      Size: "450X200X1007",
+
+      Price: 15999,
     },
     {
       id: 3,
-      items: {
-        ProductPicture: "/images/3.png",
-        ProductName: "CRYSTAL FRUIT DECOR",
-        ModelNumber: "RD5713 and RD5714",
-        Quantity: "1 set of 2",
-        Size: "80X110X280 and 80X130X250",
 
-        Price: 18999,
-      },
+      ProductPicture: "/images/3.png",
+      ProductName: "CRYSTAL FRUIT DECOR",
+      ModelNumber: "RD5713 and RD5714",
+      Quantity: "1 set of 2",
+      Size: "80X110X280 and 80X130X250",
+
+      Price: 18999,
     },
     {
       id: 4,
-      items: {
-        ProductPicture: "/images/4.png",
-        ProductName: "GLOBAL TABLE DECOR",
-        ModelNumber: "RD3964L and RD3964S",
-        Quantity: "1",
-        Size: "240X510 and 240X450",
 
-        Price: 12999,
-      },
+      ProductPicture: "/images/4.png",
+      ProductName: "GLOBAL TABLE DECOR",
+      ModelNumber: "RD3964L and RD3964S",
+      Quantity: "1",
+      Size: "240X510 and 240X450",
+
+      Price: 12999,
     },
+
     {
       id: 5,
-      items: [
-        {
-          ProductPicture: "/images/5.png",
-          ProductName: "ARTISTIC SWAN",
-          ModelNumber: "DECOR",
-          Quantity: "1",
-          Size: null,
 
-          Price: 10500,
-        },
-      ],
+      ProductPicture: "/images/5.png",
+      ProductName: "ARTISTIC SWAN",
+      ModelNumber: "DECOR",
+      Quantity: "1",
+      Size: null,
+
+      Price: 10500,
     },
     {
       id: 6,
-      items: {
-        ProductPicture: "/images/6.png",
-        ProductName: "METAL MOUNTAIN",
-        ModelNumber: "SM-Z s278 and SM-Z s279",
-        Quantity: 1,
-        Size: "280X80X320 and 280X70X270",
 
-        Price: 14999,
-      },
+      ProductPicture: "/images/6.png",
+      ProductName: "METAL MOUNTAIN",
+      ModelNumber: "SM-Z s278 and SM-Z s279",
+      Quantity: 1,
+      Size: "280X80X320 and 280X70X270",
+
+      Price: 14999,
     },
+
     {
       id: 7,
-      items: {
-        ProductPicture: "/images/7.png",
-        ProductName: "CRYSTAL FLOWER",
-        ModelNumber: "RD5712L and RD5712S",
-        Quantity: 1,
-        Size: "230X230X480 and 230X230X410",
 
-        Price: 20999,
-      },
+      ProductPicture: "/images/7.png",
+      ProductName: "CRYSTAL FLOWER",
+      ModelNumber: "RD5712L and RD5712S",
+      Quantity: 1,
+      Size: "230X230X480 and 230X230X410",
+
+      Price: 20999,
     },
     {
       id: 8,
-      items: {
-        ProductPicture: "/images/8.png",
-        ProductName: "RESIN ORNAMENT",
-        ModelNumber: "SM-Z s311 and SM-Z s312",
-        Quantity: "1 set of 2",
-        Size: "140X120X320 and 200X100X180",
 
-        Price: 18499,
-      },
+      ProductPicture: "/images/8.png",
+      ProductName: "RESIN ORNAMENT",
+      ModelNumber: "SM-Z s311 and SM-Z s312",
+      Quantity: "1 set of 2",
+      Size: "140X120X320 and 200X100X180",
+
+      Price: 18499,
     },
     {
       id: 9,
-      items: [
-        {
-          ProductPicture: "/images/9.png",
-          ProductName: "CAKE STAND SNACKS PLATTER",
-          ModelNumber: "DECOR",
-          Quantity: "1",
-          Size: "",
 
-          Price: 11999,
-        },
-      ],
+      ProductPicture: "/images/9.png",
+      ProductName: "CAKE STAND SNACKS PLATTER",
+      ModelNumber: "DECOR",
+      Quantity: "1",
+      Size: "",
+
+      Price: 11999,
     },
     {
       id: 10,
-      items: {
-        ProductPicture: "/images/10.png",
-        ProductName: "DEER STATUE",
-        ModelNumber: "RD5799L and RD5799S",
-        Quantity: "1",
-        Size: "140X210X370 and 160X70X280",
 
-        Price: 26999,
-      },
+      ProductPicture: "/images/10.png",
+      ProductName: "DEER STATUE",
+      ModelNumber: "RD5799L and RD5799S",
+      Quantity: "1",
+      Size: "140X210X370 and 160X70X280",
+
+      Price: 26999,
     },
     {
       id: 11,
-      items: {
-        ProductPicture: "/images/11.png",
-        ProductName: "DEER STATUE 2",
-        ModelNumber: "RD5543L and RD5543S",
-        Quantity: 1,
-        Size: "220X190X535 and 290X180X380",
 
-        Price: 20499,
-      },
+      ProductPicture: "/images/11.png",
+      ProductName: "DEER STATUE 2",
+      ModelNumber: "RD5543L and RD5543S",
+      Quantity: 1,
+      Size: "220X190X535 and 290X180X380",
+
+      Price: 20499,
     },
     {
       id: 12,
-      items: {
-        ProductPicture: "/images/12.png",
-        ProductName: "CRYSTAL CRANE BIRD",
-        ModelNumber: "RD6151 and RD6152",
-        Quantity: "1set of 2",
-        Size: "130X85X360 and 135X80X310",
 
-        Price: 19599,
-      },
+      ProductPicture: "/images/12.png",
+      ProductName: "CRYSTAL CRANE BIRD",
+      ModelNumber: "RD6151 and RD6152",
+      Quantity: "1set of 2",
+      Size: "130X85X360 and 135X80X310",
+
+      Price: 19599,
     },
     {
       id: 13,
-      items: {
-        ProductPicture: "/images/13.png",
-        ProductName: "LEAF ORNAMENT DECOR",
-        ModelNumber: "RD5894L and RD5894S",
-        Quantity: "1 set of 2",
-        Size: "210X120X470 and 190X180X400",
 
-        Price: 18499,
-      },
+      ProductPicture: "/images/13.png",
+      ProductName: "LEAF ORNAMENT DECOR",
+      ModelNumber: "RD5894L and RD5894S",
+      Quantity: "1 set of 2",
+      Size: "210X120X470 and 190X180X400",
+
+      Price: 18499,
     },
     {
       id: 14,
-      items: [
-        {
-          ProductPicture: "/images/14.png",
-          ProductName: "CRYSTAL GLASS HORSE",
-          ModelNumber: "SM-Zs388 and SM-Zs389",
-          Quantity: "1",
-          Size: "255X80X400 and 256X80X350",
 
-          Price: 14999,
-        },
-      ],
+      ProductPicture: "/images/14.png",
+      ProductName: "CRYSTAL GLASS HORSE",
+      ModelNumber: "SM-Zs388 and SM-Zs389",
+      Quantity: "1",
+      Size: "255X80X400 and 256X80X350",
+
+      Price: 14999,
     },
     {
       id: 15,
-      items: [
-        {
-          ProductPicture: "/images/15.png",
-          ProductName: "HALF COVERED BLACK",
-          ModelNumber: "SM-BUJ448",
-          Quantity: "1",
-          Size: "300X120X250",
 
-          Price: 7499,
-        },
-      ],
+      ProductPicture: "/images/15.png",
+      ProductName: "HALF COVERED BLACK",
+      ModelNumber: "SM-BUJ448",
+      Quantity: "1",
+      Size: "300X120X250",
+
+      Price: 7499,
     },
     {
       id: 16,
-      items: [
-        {
-          ProductPicture: "/images/16.png",
-          ProductName: "HALF COVERED WHITE HOSRE",
-          ModelNumber: "SM-BUJ448",
-          Quantity: "1",
-          Size: "300X120X250",
 
-          Price: 7499,
-        },
-      ],
+      ProductPicture: "/images/16.png",
+      ProductName: "HALF COVERED WHITE HOSRE",
+      ModelNumber: "SM-BUJ448",
+      Quantity: "1",
+      Size: "300X120X250",
+
+      Price: 7499,
     },
     {
       id: 17,
-      items: [
-        {
-          ProductPicture: "/images/17.png",
-          ProductName: "BIRD NEIGHBORS",
-          ModelNumber: "SM-Z s280 and SM-Z s281",
-          Quantity: "1 set of 2",
-          Size: "270X75X385 and 215X75X300",
 
-          Price: 18999,
-        },
-      ],
+      ProductPicture: "/images/17.png",
+      ProductName: "BIRD NEIGHBORS",
+      ModelNumber: "SM-Z s280 and SM-Z s281",
+      Quantity: "1 set of 2",
+      Size: "270X75X385 and 215X75X300",
     },
     {
       id: 18,
-      items: [
-        {
-          ProductPicture: "/images/18.png",
-          ProductName: "STUNNING METAL EAGLE",
-          ModelNumber: "RD5885L and RD5885S",
-          Quantity: "1 Set of 2",
-          Size: "130X70X150 and 120X70X210",
 
-          Price: 12499,
-        },
-      ],
+      ProductPicture: "/images/18.png",
+      ProductName: "STUNNING METAL EAGLE",
+      ModelNumber: "RD5885L and RD5885S",
+      Quantity: "1 Set of 2",
+      Size: "130X70X150 and 120X70X210",
+
+      Price: 12499,
     },
     {
       id: 19,
-      items: [
-        {
-          ProductPicture: "/images/19.png",
-          ProductName: "HORSE JAR",
-          ModelNumber: "SM-Z s268 and SM-Z s269",
-          Quantity: "1 Set of 2",
-          Size: "140X140X370 and 160X160310",
 
-          Price: 15699,
-        },
-      ],
+      ProductPicture: "/images/19.png",
+      ProductName: "HORSE JAR",
+      ModelNumber: "SM-Z s268 and SM-Z s269",
+      Quantity: "1 Set of 2",
+      Size: "140X140X370 and 160X160310",
     },
     {
       id: 20,
-      items: [
-        {
-          ProductPicture: "/images/20.png",
-          ProductName: "CERAMIC VASES",
-          ModelNumber: "RD3873S and RD3873L",
-          Quantity: "1 Set of 2",
-          Size: "230X230 amd 230X400",
 
-          Price: null,
-        },
-      ],
+      ProductPicture: "/images/20.png",
+      ProductName: "CERAMIC VASES",
+      ModelNumber: "RD3873S and RD3873L",
+      Quantity: "1 Set of 2",
+      Size: "230X230 amd 230X400",
+
+      Price: null,
     },
     {
       id: 21,
-      items: [
-        {
-          ProductPicture: "/images/21.png",
-          ProductName: "ACRYLIC PRINT DECOR",
-          ModelNumber: "RD4587L and RD4587S",
-          Quantity: "1 set of 2",
-          Size: "220X80X510 and 340X80X300",
 
-          Price: 19499,
-        },
-      ],
+      ProductPicture: "/images/21.png",
+      ProductName: "ACRYLIC PRINT DECOR",
+      ModelNumber: "RD4587L and RD4587S",
+      Quantity: "1 set of 2",
+      Size: "220X80X510 and 340X80X300",
+
+      Price: 19499,
     },
     {
       id: 22,
-      items: [
-        {
-          ProductPicture: "/images/22.png",
-          ProductName: "ONYX CANDLE STAND",
-          ModelNumber: "SM-Zs302 , SM-Zs303 and SM-Zs304",
-          Quantity: "1 set of 3",
-          Size: "70X250 , 70X200 and 70X150",
 
-          Price: 12499,
-        },
-      ],
+      ProductPicture: "/images/22.png",
+      ProductName: "ONYX CANDLE STAND",
+      ModelNumber: "SM-Zs302 , SM-Zs303 and SM-Zs304",
+      Quantity: "1 set of 3",
+      Size: "70X250 , 70X200 and 70X150",
+
+      Price: 12499,
     },
     {
       id: 23,
-      items: [
-        {
-          ProductPicture: "/images/23.png",
-          ProductName: "ABSTRACT SCULPTURE",
-          ModelNumber: "SM-Zs307 and SM-Z s308",
-          Quantity: "1 set of 2",
-          Size: "300X80X450 and 300X80X400",
 
-          Price: 17499,
-        },
-      ],
+      ProductPicture: "/images/23.png",
+      ProductName: "ABSTRACT SCULPTURE",
+      ModelNumber: "SM-Zs307 and SM-Z s308",
+      Quantity: "1 set of 2",
+      Size: "300X80X450 and 300X80X400",
+
+      Price: 17499,
     },
     {
       id: 24,
-      items: [
-        {
-          ProductPicture: "/images/24.png",
-          ProductName: "CERAMIC ORNAMENTS FLOWER VASE",
-          ModelNumber: "SM-Z282 , SM-Z283 and SM-s284",
-          Quantity: "a set of 3",
-          Size: "460X460 , 330X560 and 490X600",
 
-          Price: 29699,
-        },
-      ],
+      ProductPicture: "/images/24.png",
+      ProductName: "CERAMIC ORNAMENTS FLOWER VASE",
+      ModelNumber: "SM-Z282 , SM-Z283 and SM-s284",
+      Quantity: "a set of 3",
+      Size: "460X460 , 330X560 and 490X600",
+
+      Price: 29699,
     },
 
     {
       id: 25,
-      items: [
-        {
-          ProductPicture: "/images/25.png",
-          ProductName: "CERAMIC VASES",
-          ModelNumber: "RD3875S and RD3875L",
-          Quantity: 1,
-          Size: "200X100X350 and 200X100X360",
 
-          Price: 18299,
-        },
-      ],
+      ProductPicture: "/images/25.png",
+      ProductName: "CERAMIC VASES",
+      ModelNumber: "RD3875S and RD3875L",
+      Quantity: 1,
+      Size: "200X100X350 and 200X100X360",
+
+      Price: 18299,
     },
   ],
 };
