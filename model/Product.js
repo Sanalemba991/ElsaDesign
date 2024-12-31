d52ec8b4cbbe58fab09c58d58c48acb1dcd5e63e
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-31 11:31:14 +0530

    product.js also fixed

--- model/Product.js
+++ model/Product.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   id: Number,
-  ProductPicture: {
-    type: String,
-    match: [/^https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg)$/, 'Please fill a valid URL'],
-    required: [true, 'Product picture URL is required'],
-  },
+
+  ProductPicture: String,
   Name: String,
   ProductName: String,
+  ModelNumber: String,
   Quantity: String,
   Size: String,
   OnlinePrice: { type: String, default: null },
